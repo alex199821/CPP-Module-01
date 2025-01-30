e7cf00da5dd63112d6059632519b4b923906f7fc
auplisas <aupli16@freeuni.edu.ge>
2025-01-30 23:38:16 +0100

    add: ex04 in progress

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -5,13 +5,17 @@
 
 int	main(int argc, char **argv)
 {
+	std::string single_line = "";
+	std::string saved_buffer = "";
+	std::string test_str = "";
+	size_t newline_pos;
 	if (argc != 4)
 	{
 		std::cout << "Wrong Number of Arguments." << std::endl;
 	}
 	std::string fileName = argv[1];
-	std::string s1 = argv[2];
-	std::string s2 = argv[3];
+	std::string str_to_find = argv[2];
+	std::string str_to_replace = argv[3];
 
 	std::ifstream src(fileName, std::ios::binary);
 	if (!src)
@@ -22,21 +26,42 @@ int	main(int argc, char **argv)
 
     std::ofstream dest("destination.txt", std::ios::binary | std::ios::trunc);
 
-    char buffer[1024];
+    char buffer[5];
 
     while(src.read(buffer, sizeof(buffer)))
     {
-        dest.write(buffer, src.gcount());
+		single_line.append(buffer, src.gcount());
+		if(single_line.find("\n") != std::string::npos)
+		{
+			newline_pos = single_line.find('\n');
+			
+			test_str.append(saved_buffer);
+			saved_buffer.clear();
+			test_str = single_line.substr(0,newline_pos);
+			
+			saved_buffer = single_line.substr(newline_pos + 1);
+			// std::cout << saved_buffer << std::endl;
+			std::cout << test_str << std::endl;
+			single_line.clear();
+		}
+		// single_line.clear();
+        // dest.write(buffer, src.gcount());
+    }
+	
+	if (src.gcount() > 0)
+    {
+        single_line.append(buffer, src.gcount());
     }
 
-    if(src.gcount() > 0)
-    {
-        dest.write(buffer, src.gcount());
-    }
+    // if(src.gcount() > 0)
+    // {
+    //     dest.write(buffer, src.gcount());
+    // }
 
 
     src.close();
     dest.close();
+	// std::cout << single_line << std::endl;
 
 	return (0);
 }
